b9cbb0e21e07a728c4e4f2c303ed36fdc67b0922
miki4920 <miki4920@gmail.com>
2023-10-17 23:40:57 +0100

    Allowed for listing members from the database

---
+++ Application/Views/list-members.php
@@ -0,0 +1,24 @@
+<main class="container">
+    <div class="jumbotron">
+        <h1>Search Members by School</h1>
+        <form method="post" action="/api/member" id="searchForm">
+            <div class="form-group">
+                <label for="schoolSelect">Select School:</label>
+                <select class="form-control" id="schoolSelect" name="school_id" required>
+                    <option value="">--Select a school--</option>
+                    <?php if (!empty($schools)) {
+                        foreach ($schools as $school): ?>
+                            <option value="<?php echo htmlspecialchars($school['id']); ?>">
+                                <?php echo htmlspecialchars($school['school_name']); ?>
+                            </option>
+                        <?php endforeach;
+                    } ?>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Search</button>
+        </form>
+        <div id="results">
+
+        </div>
+    </div>
+</main>
