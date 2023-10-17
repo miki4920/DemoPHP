f67833c001efb9973c7431ea324bf6b6491bf5e1
miki4920 <miki4920@gmail.com>
2023-10-17 23:40:23 +0100

    Added basic header and footer tempalates with necessary js

---
+++ Application/Views/header-template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cover Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/css/bootstrap-select.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/index.css">
+</head>
+<body>
+
+<nav>
+    <div class="center">
+        <a class="navbar-brand" href="/">
+            <img src="/images/icon.svg" width="40" height="40" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="/pages/add-school">Add School</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/pages/add-member">Add Member</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/pages/list-members">List Members</a>
+            </li>
+        </ul>
+    </div>
+</nav>
