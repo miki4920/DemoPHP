07801e3641622674363054e4b39466ca2ec65528
miki4920 <miki4920@gmail.com>
2023-10-18 11:25:39 +0100

    Added default main page

--- Application/Views/index.php
+++ Application/Views/index.php
@@ -1,5 +1,8 @@
 <main class="container">
     <div class="jumbotron">
-
+        <h1 class="h3 mb-3 mt-3 font-weight-normal">Lorem Ipsum</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt, nisl vel sollicitudin commodo, libero turpis dignissim lacus, sit amet dictum quam lorem eget est. Sed eget enim feugiat, finibus nunc ac, dapibus nunc. Aliquam erat volutpat.
+        </p>
     </div>
 </main>
